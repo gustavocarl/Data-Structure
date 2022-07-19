0a9d0b756b33882fd5206b04c6e1ae6ac9920093
Gustavo Henrique Carl Dos Santos <gustavo_carl@live.com>
2022-07-19 20:30:13 -0300

    Add Data Structures Binary Search and Selection Sort

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Binary Search\n");
+
+int low = 0, attempt = 0, kick, middle;
+
+Console.WriteLine("Informe o tamanho da lista");
+int listLength = int.Parse(Console.ReadLine());
+
+int[] list = new int[listLength + 1];
+
+Console.WriteLine("Informe o elemento que deve ser encontrado na lista");
+int searchedElement = int.Parse(Console.ReadLine());
+
+int high = list.Length - 1;
+
+for (int i = 1; i <= list.Length - 1; i++)
+{
+    list[i] += i;
+}
+
+Console.WriteLine($"\n{searchedElement} elemento que deve ser encontrado\n");
+
+do
+{
+    attempt++;
+    middle = (low + high) / 2;
+    kick = list[middle];
+
+    if (kick < searchedElement)
+        low = middle + 1;
+    else if (kick > searchedElement)
+        high = middle - 1;
+
+    Console.WriteLine($"{attempt}ª Tentativa: {middle}");
+} while (middle != searchedElement);
+Console.ReadLine();
---
+++ SelectionSort/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Selection Sort");
+int[] arrayNumber = new int[10] { 7, 4, 3, 6, 2, 9, 1, 5, 8, 0 };
+
+for (int i = 0; i < arrayNumber.Length - 1; i++)
+{
+    int aux = i;
+    for (int j = i; j < arrayNumber.Length; j++)
+    {
+        if (arrayNumber[j] < arrayNumber[aux])
+            aux = j;
+
+        int temp = arrayNumber[aux];
+        arrayNumber[aux] = arrayNumber[i];
+        arrayNumber[i] = temp;
+    }
+}
+
+for (int i = 0; i < arrayNumber.Length; i++)
+{
+    Console.Write(arrayNumber[i] + " ");
+}
